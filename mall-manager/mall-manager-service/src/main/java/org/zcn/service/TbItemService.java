8567880a84ae3f803c741e532cfe1908f18f5c6e
tony1989zhang <981601917@qq.com>
2020-06-15 11:16:46 +0800

    提交

--- mall-manager/mall-manager-service/src/main/java/org/zcn/service/TbItemService.java
+++ mall-manager/mall-manager-service/src/main/java/org/zcn/service/TbItemService.java
@@ -3,6 +3,6 @@ package org.zcn.service;
 import org.zcn.utils.EasyUIDataGridResult;
 
 public interface TbItemService {
-	//获取商品列表
+	//获取商品列表 测试
 	public EasyUIDataGridResult getTbItemList(Integer page,Integer rows);
 }
