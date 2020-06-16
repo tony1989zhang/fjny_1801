294452e3039e3e11c8f43a12ea7a76d84e6b6528
tony1989zhang <981601917@qq.com>
2020-06-16 14:28:39 +0800

    提交EasyUITreeNodeBean

---
+++ mall-common/src/main/java/org/zcn/utils/EasyUITreeNodeBean.java
@@ -0,0 +1,26 @@
+package org.zcn.utils;
+
+public class EasyUITreeNodeBean {
+	private long id;
+	private String text;
+	private String open;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getOpen() {
+		return open;
+	}
+	public void setOpen(String open) {
+		this.open = open;
+	}
+	
+}
